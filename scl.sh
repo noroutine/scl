816d02e5e9d334002513dcf8e68969a454cb7983
Oleksiy Khilkevich <o.khilkevich@gastrofix.com>
2013-09-16 20:22:23 +0200

    add support for passing java  -X and -D arguments to java runtime

--- scl.sh
+++ scl.sh
@@ -2,9 +2,21 @@
 
 M2_REPO=$HOME/.m2/repository
 
+java_args=()
+prog_args=()
+
+for arg in "$@"; do
+    if [[ $arg =~ ^(-X|-D) ]]; then
+        java_args=( "${java_args[@]}" $arg )
+    else
+        prog_args=( "${prog_args[@]}" $arg )
+    fi
+done
+
 CLASSPATH=$M2_REPO/commons-cli/commons-cli/1.2/commons-cli-1.2.jar:$M2_REPO/org/linuxgears/scl/1.0-SNAPSHOT/scl-1.0-SNAPSHOT.jar
 
 java -Dorg.linuxgears.scl.java.compiler=$JAVA_HOME/bin/javac \
     -cp $CLASSPATH \
+    "${java_args[@]}" \
     org.linuxgears.App \
-    $@
+    "${prog_args[@]}"
